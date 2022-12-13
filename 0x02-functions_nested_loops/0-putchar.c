45a3090055657ea97966073f619dc8a8dbb4e664
MrBrianMaina <62014227+MrBrianMaina@users.noreply.github.com>
2022-12-13 10:26:00 -0800

    putchar compex -1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * main - Prints Holberton as a message.
+ * main - Prints the _putchar.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('\n');
 	return (0);
 }
