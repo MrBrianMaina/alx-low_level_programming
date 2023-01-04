c6282a94e52df417381ba3e15af4b4e2322b2631
MrBrianMaina <62014227+MrBrianMaina@users.noreply.github.com>
2023-01-03 17:36:17 -0800

    Pointer of pointers -1

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 		bool = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(s + 1) == *(accept + j))
+			if (*(s + i) == *(accept + j))
 			{
 			bool = 0;
 			break;
